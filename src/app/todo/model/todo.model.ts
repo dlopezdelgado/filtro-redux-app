6acfb471c28c2c428bf84097c37e7765b32dbca8
Daniel Lopez <dlopezdelgado@gmail.com>
2020-02-21 17:08:46 -0500

    Primer commit

---
+++ src/app/todo/model/todo.model.ts
@@ -0,0 +1,14 @@
+
+
+export class Todo {
+    public id: number;
+    public texto: string;
+    public completado: boolean;
+
+    constructor(texto: string) {
+        this.texto = texto;
+        this.completado = false;
+
+        this.id = Math.random();
+    }
+}
